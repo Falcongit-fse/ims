d195ce884d6133ba91d140c5e1024b7db3e7ebe1
cognizantjava14 <cognizantjava14@cogjava128.iiht.tech>
2020-08-03 22:55:38 +0530

    adad

--- src/test/java/com/inventory/inventory/InventoryApplicationTests.java
+++ src/test/java/com/inventory/inventory/InventoryApplicationTests.java
@@ -16,10 +16,13 @@ import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.inventory.ProductApplication;
 import com.inventory.controller.ProductController;
 import com.inventory.entity.Product;
+import com.inventory.service.MyUserDetailsService;
 import com.inventory.service.ProductService;
 @RunWith(MockitoJUnitRunner.class)
 @SpringBootTest(classes=ProductApplication.class)
@@ -29,6 +32,7 @@ public class InventoryApplicationTests {
 	ProductController controller;
     @Mock
     ProductService service;
+    
     @Before
     public void setUp() {
     	MockitoAnnotations.initMocks(this);
@@ -79,6 +83,56 @@ public class InventoryApplicationTests {
     	ResponseEntity<String> result = controller.deleteProduct(product.getProductId());
     	assertEquals("Product deleted successfully!!", result.getBody());
     }
+    @Test
+    public void loadUserByUsernameTest() {
+    	User user = new User("susmitha", "susmitha123", new ArrayList<>());
+    	MyUserDetailsService userService=new MyUserDetailsService();
+    	UserDetails userDetails=userService.loadUserByUsername("susmitha");
+    	assertEquals(userDetails, user);
+    	
+    }
     
+    @Test
+    public void getProductIdTest() {           
+        Integer productId=1;
+        Product product=new Product();
+        product.setProductId(productId);
+        assertEquals(product.getProductId(), productId);
+    }
+    @Test
+    public void getProductNameTest() {           
+        String productName="Mobile";
+        Product product=new Product();
+        product.setProductName(productName);;
+        assertEquals(product.getProductName(), productName);
+    }
+    @Test
+    public void getProductTypeTest() {           
+        String productType="Finished Product";
+        Product product=new Product();
+        product.setProductType(productType);
+        assertEquals(product.getProductType(), productType);
+    }
+    @Test
+    public void getProductQuantityTest() {           
+        Integer productQuantity=10;
+        Product product=new Product();
+        product.setProductQuantity(productQuantity);;
+        assertEquals(product.getProductQuantity(), productQuantity);
+    }
+    @Test
+    public void getProductPriceTest() {           
+        Double productPrice=10000.0;
+        Product product=new Product();
+        product.setProductPrice(productPrice);
+        assertEquals(product.getProductPrice(), productPrice);
+    }
+    @Test
+    public void getProductSoldTest() {           
+        Integer productSold=10;
+        Product product=new Product();
+        product.setProductsSold(productSold);
+        assertEquals(product.getProductsSold(), productSold);
+    }
 
 }
